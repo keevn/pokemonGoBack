436963fb63732eca4decee501e1f5de5793f4352
keevn <keevn@163.com>
2018-07-05 16:04:47 -0400

    add initial ai user

--- Server/src/main/resources/import.sql
+++ Server/src/main/resources/import.sql
@@ -1,6 +1,6 @@
 INSERT INTO pokemon_go_back.roles (id, name) VALUES (2, 'ROLE_ADMIN');
 INSERT INTO pokemon_go_back.roles (id, name) VALUES (1, 'ROLE_USER');
-INSERT INTO pokemon_go_back.users (id, created_at, updated_at, email, name, password, username, default_deck_id) VALUES
-  (1, '2018-07-05 18:31:54', '2018-07-05 18:31:55', 'ai@pokemongoback.com', 'pokemonai',
-   '$2a$10$y/NYCsjD75NSVLVlPSs8uuqvSOechIYKOLax/IyTqCIx9yus.Xax6', 'pokemonai', null);
+INSERT INTO pokemon_go_back.users (created_at, updated_at, email, name, password, username) VALUES
+  (now(), now(), 'ai@pokemongoback.com', 'pokemonai', '$2a$10$y/NYCsjD75NSVLVlPSs8uuqvSOechIYKOLax/IyTqCIx9yus.Xax6',
+   'pokemonai');
 INSERT INTO pokemon_go_back.user_roles (user_id, role_id) VALUES (1, 1);
